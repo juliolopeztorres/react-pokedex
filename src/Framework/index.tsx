60628fc07956acb96c4271606c20d6ba59ec1124
Julio López <juliolopeztorres@gmail.com>
2023-06-13 18:54:05 +0200

    feat: Add 'tailwindcss' base components when loading the app

--- src/Framework/index.tsx
+++ src/Framework/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import RouteService from "./Service/RouteService";
 
+import './styles.css';
+
 const anchor: HTMLElement | null = document.getElementById('app')
 
 if (!anchor) {
