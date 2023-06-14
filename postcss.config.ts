e377907a4eddd5ac13ea42e2f296540a01a3f051
Julio López <juliolopeztorres@gmail.com>
2023-06-14 17:28:14 +0200

    feat: Add 'cssnano' dependency and optimize 'tailwindcss' build on ENV == prod

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,13 @@
+
+let plugins = [
+  'postcss-preset-env',
+  'tailwindcss',
+]
+
+if (process.env.ENV === 'prod') {
+  plugins.push('cssnano')
+}
+
 export default {
-  plugins: [
-    'postcss-preset-env',
-    'tailwindcss'
-  ],
+  plugins: plugins
 }
